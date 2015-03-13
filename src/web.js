7df257935a03cc0d564dc4f413980ea8e492f29a
MiniKeb <code@rabiller.com>
2015-03-13 17:19:38 +0100

    [First Web Service] Let's try

--- src/web.js
+++ src/web.js
@@ -9,6 +9,10 @@ app.get('/', function(req, res) {
   res.send('Hello Yose');
 });
 
+app.get('/ping', function(request, response){
+  response.send('{ "alive" : true }')
+});
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
