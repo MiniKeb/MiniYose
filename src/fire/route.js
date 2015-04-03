7c1428369fe1504d055b771420b553962cdd68e3
unknown <sebastien.rabiller@CD000141.cdbdx.biz>
2015-04-03 10:57:31 +0200

    spaces -> tabs indentation

--- src/fire/route.js
+++ src/fire/route.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var run = require(__dirname + "/fire.js");
 
 router.get("/geek", function(request, response){
-  var width = request.param("width");
-  var map = request.param("map");
-  var result = run.fireGeek(width, map);
-  response.json(result);
+	var width = request.param("width");
+	var map = request.param("map");
+	var result = run.fireGeek(width, map);
+	response.json(result);
 });
 
 module.exports = router;
