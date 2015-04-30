22959a177cb8c5d8de0df782dab8f0c6c9ef649b
unknown <sebastien.rabiller@CD000141.cdbdx.biz>
2015-04-30 17:10:41 +0200

    [Refactor Fire] Let's use visitors!

--- src/fire/fire.js
+++ src/fire/fire.js
@@ -1,72 +1,132 @@
 module.exports.fireGeek = function(widthTxt, flatMap){
 	var width = parseInt(widthTxt);
-	var map = getMap(width, flatMap);
 
-	var positions = getPositions(map);
+	var data = new CombineVisitor();
+	visitFlatMap(flatMap, width, data);
 
-	// Ok, that's odd but I need a clone and I don't wanna go all over several times.
-	var grids = buildGrids(width, flatMap);
+	var positions = data.getPositions();
 
-	var best = positions.water[0];
-	var bestScore = calculateScore(positions.plane, best);
-	if(positions.fire)
-		bestScore += calculateScore(best, positions.fire);
-
-	for(var w = 0; w < positions.water.length; w++){
-		var score = calculateScore(positions.plane, positions.water[w]);
-		if(positions.fire)
-			score += calculateScore(positions.water[w], positions.fire);
-		
-		if(bestScore > score){
-			best = positions.water[w];
-			bestScore = score;
-		}
-	}
-
-	var moves = getTakeWater(grids[0], positions.plane, best);
+	var best = getBestWaterPosition(positions);
+	var moves = getPath(data.getWaterGrid(), positions.plane, best, "F");
 	if(positions.fire)
 	{
-		moves = moves.concat(getExtinguishFire(grids[1], best, positions.fire));
+		moves = moves.concat(getPath(data.getFireGrid(), best, positions.fire));
 	}
 
 	return {
-		map: map,
+		map: data.getMap(),
 		moves : moves
 	}
 }
 
-function buildGrids(width, flatMap){
-	var grids = [];
+var CombineVisitor = function(){
+	this.map = new MapVisitor();
+	this.positions = new PositionsVisitor();
+	this.waterGrid = new GridVisitor();
+	this.fireGrid = new GridVisitor();
 
-	for(var i = 0; i < 2; i++){
-		var grid = [];
-		var y = 0;
-
-		while(y * width < flatMap.length){
-			grid.push([]);
-			grid[y] = [];
-
-			for(var x = 0; x < width; x++){
-				var node = {
-					parent: null,
-					value : flatMap[(y * width) + x],
-					position : {x: x, y: y},
-					isVisited : false,
-
-					isFire : function(){ return this.value == "F" }
-				}
-				grid[y].push(node);
-			}
-
-			y++;
+	this.visitors = [this.map, this.positions, this.waterGrid, this.fireGrid];
+};
+CombineVisitor.prototype = {
+	visit: function(value, line, row){
+		for(var v = 0; v < this.visitors.length; v++){
+			this.visitors[v].visit(value, line, row);
 		}
+	}, 
+	getMap: function(){ return this.map.map; },
+	getPositions: function(){ return this.positions.positions; },
+	getWaterGrid: function(){ return this.waterGrid.grid; },
+	getFireGrid: function(){ return this.fireGrid.grid; }
+}
+
+var MapVisitor = function(){
+	this.map = [];
+};
+MapVisitor.prototype = {
+	visit: function(value, line, row){
+		if(!this.map[line])
+			this.map.push("");
+
+		this.map[line] = this.map[line] + value;
+	}
+};
+
+var PositionsVisitor = function(){
+	this.positions = {
+		plane: undefined,
+		waters: [],
+		fire: undefined
+	};
+};
+PositionsVisitor.prototype = {
+	visit: function(value, line, row){
+		var sign = value;
+		switch(sign)
+		{
+			case "P" : this.positions.plane = {x: row, y: line};
+				break;
+			case "W" : this.positions.waters.push({x: row, y: line});
+				break;
+			case "F" : this.positions.fire = {x: row, y: line};
+				break;
+			default:
+				break;
+		}
+	}
+};
+
+var GridVisitor = function(){
+	this.grid = [];
+};
+GridVisitor.prototype = {
+	visit: function(value, line, row){
+		if(!this.grid[line]){
+			this.grid.push([]);
+			this.grid[line] = [];
+		}
+
+		var node = {
+			parent: null,
+			value : value,
+			position : {x: row, y: line},
+			isVisited : false
+		}
+		this.grid[line].push(node);
+	}	
+};
+
+function visitFlatMap(flatMap, width, visitor){
+	var line = 0;
+	while(line * width < flatMap.length){
+		for(var row = 0; row < width; row++){
+			var value = flatMap[(line * width) + row];
+			visitor.visit(value, line, row);
+		}
+		line++;
+	}
+}
+
+function getBestWaterPosition(positions){
+	var best = positions.waters[0];
+	var bestScore = calculateScore(positions.plane, best);
+	if(positions.fire)
+		bestScore += calculateScore(best, positions.fire);
+
+	for(var w = 0; w < positions.waters.length; w++){
+		var score = calculateScore(positions.plane, positions.waters[w]);
+		if(positions.fire)
+			score += calculateScore(positions.waters[w], positions.fire);
 		
-		grids.push(grid);
+		if(bestScore > score){
+			best = positions.waters[w];
+			bestScore = score;
+		}
 	}
-	return grids;
+
+	return best;
 }
 
-function getTakeWater(map, startPos, endPos){
+function getPath(map, startPos, endPos, cellToAvoid){
 	var currentNode = map[startPos.y][startPos.x];
 	currentNode.isVisited = true;
 
@@ -79,7 +139,8 @@ function getTakeWater(map, startPos, endPos){
 		var neighbourgs = getNeighbourgs(map, currentNode.position);
 		for(var i = 0; i < neighbourgs.length; i++){
 			var node = neighbourgs[i];
-			if(!node.isFire() && !node.isVisited){
+			var canPass = !cellToAvoid || (node.value != cellToAvoid)
+			if(!node.isVisited && canPass){
 				var score = calculateScore(node.position, endPos);
 				if(bestNeighbourg.node == null || bestNeighbourg.score > score){
 					bestNeighbourg.node = node;
@@ -113,63 +174,6 @@ function getTakeWater(map, startPos, endPos){
 	});
 
 	return reverse(moves);
-}
-
-function getExtinguishFire(map, startPos, endPos){
-	var currentNode = map[startPos.y][startPos.x];
-	currentNode.isVisited = true;
-
-	while(!isSamePosition(currentNode.position, endPos)){
-		var bestNeighbourg = {
-			node: null,
-			score: 0
-		};
-
-		var neighbourgs = getNeighbourgs(map, currentNode.position);
-		for(var i = 0; i < neighbourgs.length; i++){
-			var node = neighbourgs[i];
-			if(!node.isVisited){
-				var score = calculateScore(node.position, endPos);
-				if(bestNeighbourg.node == null || bestNeighbourg.score > score){
-					bestNeighbourg.node = node;
-					bestNeighbourg.score = score;
-				}
-				node.isVisited = true;
-			}
-		}
-
-		bestNeighbourg.node.parent = currentNode;
-		currentNode = bestNeighbourg.node;
-	}
-
-	var moves = [];
-	var prevPosition = null;
-
-	while(currentNode.parent != null){
-		if(prevPosition != null){
-			moves.push({
-				dx: prevPosition.x - currentNode.position.x,
-				dy: prevPosition.y - currentNode.position.y
-			});
-		}
-		prevPosition = currentNode.position;
-		currentNode = currentNode.parent;
-	}
-
-	moves.push({
-		dx: prevPosition.x - currentNode.position.x,
-		dy: prevPosition.y - currentNode.position.y
-	});
-
-	return reverse(moves);
-}
-
-function reverse(array){
-	var reversed = [];
-	for(var i = array.length - 1; i >= 0; i--){
-		reversed.push(array[i]);
-	}
-	return reversed;
 }
 
 function getNeighbourgs(map, position, rejectFunc){
@@ -202,60 +206,19 @@ function isSamePosition(firstPos, secondPos){
 	return firstPos.x == secondPos.x && firstPos.y == secondPos.y;
 }
 
-function addMoves(moves, fromPos, toPos){
-	var horizontalCount = toPos.y - fromPos.y;
-	for(var y = 0; y < Math.abs(horizontalCount); y++){
-		var step = horizontalCount < 0 ? -1 : 1;
-		moves.push({dx:0, dy:step});
+function reverse(array){
+	var reversed = [];
+	for(var i = array.length - 1; i >= 0; i--){
+		reversed.push(array[i]);
 	}
-
-	var verticalCount = toPos.x - fromPos.x;
-	for(var x = 0; x < Math.abs(verticalCount); x++){
-		var step = verticalCount < 0 ? -1 : 1;
-		moves.push({dx:step, dy:0});
-	}
+	return reversed;
 }
 
 
-function getMap(width, flatMap)
-{
-	var map = [];
-	var i = 0;
-
-	while(i * width < flatMap.length)
-	{
-		map.push(flatMap.substr(i * width, width));
-		i++;
-	}
-
-	return map;
-}
 
 
-function getPositions(map)
-{
-	var positions = {
-		plane: undefined,
-		water: [],
-		fire: undefined
-	};
-	for(var y = 0; y < map.length; y++)
-	{
-		for(var x = 0; x < map[y].length; x++)
-		{
-			var sign = map[y][x];
-			switch(sign)
-			{
-				case "P" : positions.plane = {x : x, y: y};
-					break;
-				case "W" : positions.water.push({x : x, y: y});
-					break;
-				case "F" : positions.fire = {x : x, y: y};
-					break;
-				default:
-					break;
-			}
-		}
-	}
-	return positions;
-}
+
+
+
+
+
